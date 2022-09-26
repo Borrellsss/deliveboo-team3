03afebc79d2970a6cda3839ad1579df1bfbca77e
Borrellsss <edoardofilippo.borrello@gmail.com>
2022-09-26 18:58:35 +0200

    admin home done

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')
+->prefix('admin')
+->namespace('Admin')
+->name('admin.')
+->group(function(){
+    Route::get('/', 'HomeController@index')->name('home');
+});
