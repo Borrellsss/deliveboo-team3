03bcc2a05260bd2364cef90b4e9b8bb27a580ed2
Borrellsss <edoardofilippo.borrello@gmail.com>
2022-09-30 12:11:11 +0200

    changed h1 in create

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <section>
-        <h1>Crea il Tuo Nuovo Piatto</h1>
+        <h1>Aggiungi prodotto</h1>
 
         <form action="{{route('admin.products.store')}}" enctype="multipart/form-data" method="post">
             @csrf
