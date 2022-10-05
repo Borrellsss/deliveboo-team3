39940b97d4617802a8365915c854188ac9cdafdd
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-10-05 18:21:48 +0200

    Braintree back-end logic tested

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,10 @@ Route::get('/user', 'Api\UserRestaurantController@allRestaurant');
 // creo la route per per la visione di tutti i post con vuejs
 Route::get('/{id}/menu', 'Api\ProductController@restaurantMenu');
 
-
-
+// Rotte per la gestione dei pagamenti con braintree:
+// Rotta che genera il token
+Route::get('orders/generate', 'Api\Orders\OrderController@generate');
+// Rotta che effettua la transazione
+Route::post('orders/make/payment', 'Api\Orders\OrderController@makePayment');
 
 
