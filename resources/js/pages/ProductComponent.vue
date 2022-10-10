7773ec0ec9b93ec57c5f56934215e1a98423e02a
jacoposergio <djago32@gmail.com>
2022-10-10 23:32:01 +0200

    fixed header

--- resources/js/pages/ProductComponent.vue
+++ resources/js/pages/ProductComponent.vue
@@ -1,5 +1,50 @@
 <template>
     <div>
+        <div class="carrello-tutto-schermo" :class="{'ms_visible' : toggle_popup}">
+                        <div v-if="cart.length > 0" class="cart-container">
+                            <!-- v-if="cart.length > 0" -->
+                            <h3>Carrello</h3>
+                            <a @click="showProductInfo()">torna ai prodotti</a>
+                            <div v-for="(product, index) in cart" :key="index" class="row">
+                                <div class="col col-6">
+                                    <div class="product-name d-flex justify-content-center align-items-center">
+                                        {{product.name}}
+                                    </div>
+                                </div>
+                                <div class="col col-3">
+                                    <div class="product-quantity-remove d-flex flex-column justify-content-center align-items-center">
+                                        <div class="product-quantity d-flex justify-content-center align-items-center">
+                                            <a class="quantity-btn" @click='decreaseQuantity(product, index)'><span class="decrease">-</span></a>
+                                            <span class="quantity-number">{{product.quantity}}</span>
+                                            <a class="quantity-btn" @click='addItem(product)'><span class="increase">+</span></a>
+                                        </div>
+                                        <a class="remove-btn" @click='deleteItem(index)'>Rimuovi</a>
+                                    </div>
+                                </div>
+                                <div class="col col-3">
+                                    <div class="product-price d-flex flex-column justify-content-center align-items-center">
+                                        {{ product.price * product.quantity }}&euro;
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row d-flex justify-content-end">
+                                <div class="col col-12">
+                                    <div class="checkout">
+                                        <div class="total">
+                                            <div>
+                                                <div class="Subtotal">Sub-Totale</div>
+                                                <div class="items" @click="clearCart(index)">Svuota carrello</div>
+                                            </div>
+                                            <!-- <div v-for="product, index in products" :key="product.id" class="total-amount">
+                                                {{ product.price * product.quantity + index-1}}&euro;
+                                            </div> -->
+                                            <div class="total-amount">&euro;{{total_amount}}</div>
+                                        </div>
+                                    <button class="button">Checkout</button></div>
+                                </div>
+                            </div>
+                        </div>
+             </div>  
         <section>
             <div class="jumbotron jumbotron-fluid" style="margin-top:90px">
             <div class="container d-flex justify-content-around">
@@ -12,7 +57,7 @@
             </div>
             <div class="pr_container" style="margin-top:100px">
 
-                <a v-if="cart.length" class="floating-cart">
+                <a v-if="cart.length" class="floating-cart" @click="showProductInfo()">
                     <div class="count-float">
                         <span>{{cart.length}}</span>
                     </div>
@@ -44,7 +89,8 @@
                                         </div>
                                     </div>
                                      
-                                    <div class="info-popup" style="margin-top:90px"  :class="{'ms_visible' : toggle_popup}">
+                                    <div class="info-popup" style="margin-top:90px">
+                                        <!-- <div class="info-popup" style="margin-top:90px"  :class="{'ms_visible' : toggle_popup}"> -->
                                         <div id="popup1" class="overlay">
                                             <div v-for="element,index in myProduct" :key="index">
                                                 <div class="popup">
@@ -416,6 +462,29 @@
     <style lang="scss" scoped>
     @import '../style/variables';
     @import '../style/common';
+
+.carrello-tutto-schermo{
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: rgba(0, 0, 0, 0.7);
+    opacity: 1;
+    z-index: 10;
+    overflow-y: hidden;
+    display: none;
+
+    &.ms_visible{
+        display: block;
+    }
+
+    .cart-container{
+        width: 60%;
+        margin: 200px auto;
+        background-color: white;
+    }
+}
 
 .cart-col{
       position:fixed;
