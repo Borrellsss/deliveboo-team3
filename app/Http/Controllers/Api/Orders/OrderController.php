100458bcff107d726fccf0d28948fc98fefc000d
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-10-11 20:28:29 +0200

    Save form data to orders table

--- app/Http/Controllers/Api/Orders/OrderController.php
+++ app/Http/Controllers/Api/Orders/OrderController.php
@@ -8,6 +8,10 @@ use Braintree\Gateway;
 use App\Http\Requests\Orders\OrderRequest;
 use Illuminate\Support\Facades\DB;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Validator;
+use App\User;
+use App\Order;
+use App\Product;
 
 class OrderController extends Controller
 {
@@ -54,14 +58,36 @@ class OrderController extends Controller
     public function store(Request $request, Faker $faker) {
 
         // salvo i dati dell'utente
-        // $data = $request->all();
+        $data = $request->all();
         
+        // Ricalcolo l'importo totale dell'ordine(in quanto lato fronte-end è modificabile)
+        $total_amount = 0;
+
         // Salvo in una variabile ogni singolo dato
-        // $order_cart = $data['order_cart'];
-        // $customer_name = $data['order_customerName'];
-        // $customer_email = $data['order_customerEmail'];
-        // $customer_phone = $data['order_customerPhoneNumber'];
-        // $customer_address = $data['order_customerAddress'];
+        $order_cart = $data['order_cart'];
+        $customer_name = $data['order_customerName'];
+        $customer_mail = $data['order_customerEmail'];
+        $customer_phone = $data['order_customerPhoneNumber'];
+        $customer_address = $data['order_customerAddress'];
+        $user_id = $order_cart[0]['user_id'];
+
+        for ($i=0; $i < count($order_cart); $i++) { 
+            $total_amount += floatval($order_cart[$i]['price']) * intval($order_cart[$i]['quantity']);
+        }
+
+        $products_ids_array = [];
+
+        // Salvo in un array tutti gli id dei prodotti nel carrello
+        for($i = 0; $i < count($order_cart); $i++) {
+            $products_ids_array[] = $order_cart[$i]['id'];
+        }
+
+        $product_quantity_array = [];
+
+        for($i = 0; $i < count($order_cart); $i++) {
+            $product_quantity_array[] = $order_cart[$i]['quantity'];
+        }
+
 
         // Salvo in una variabile la query raw con  tutti gli numeri ordine dalla tabella "orders" 
         $sql_query = 'SELECT order_number FROM orders';
@@ -69,15 +95,16 @@ class OrderController extends Controller
         $order_numbers = DB::select($sql_query);
         
         $order_number_array = [];
-  
+        
+        // ciclo tutti i numeri d'ordine e li conservo in un array
         for($i = 0; $i < count($order_numbers); $i++) {
             $order_number_array[] = $order_numbers[$i]->order_number;
         }
-       
-
+        
         $start_condition = true;
         $order_number;
 
+        // genero delle numero random e li salvo nell'array a condizione che esistano già nell'array
         while ($start_condition) {
             // genero un numero casuale unico per il numero d'ordine
             $order_number = $faker->numberBetween(100000000000000, 999999999999999);
@@ -87,14 +114,26 @@ class OrderController extends Controller
             } 
         }
 
-        $validator = Validator::make($data, [
-            'customer_name' => 'required|max:255',
-            'customer_email' => 'required|email|max:255',
-            'customer_phone_number' => 'required|max:30',
-            'address' => 'required|max:60000'
-        ]);
+        // Salvo i dati nel database
+        $new_order = new Order();
+        $new_order->user_id = $user_id;
+        $new_order->total_amount = $total_amount;
+        $new_order->order_number = $order_number;
+        $new_order->customer_name = $customer_name;
+        $new_order->customer_mail = $customer_mail;
+        $new_order->customer_address = $customer_address;
+        $new_order->customer_phone_number = $customer_phone;
+        $new_order->save();
 
-        return response()->json();
+        // Eseguo un ciclo for per la lunghezza di "$products_ids_array" e per ogni iterazione 
+        // associo l'id del prodotto presente in "$products_ids_array" e la sua relativa quantità salvata in "$product_quantity_array"
+        for ($i=0; $i < count($products_ids_array); $i++) { 
+            $new_order->products()->attach($products_ids_array[$i], ['quantity' => $product_quantity_array[$i]]);
+        }
+
+        return response()->json([
+            'success' => true
+        ]);
     }
 }
 
--- app/Order.php
+++ app/Order.php
@@ -14,11 +14,4 @@ class Order extends Model
     public function user_order() {
         return $this->belongsTo('App\User');
     }
-
-    protected $fillable = [
-        'customer_name' => 'required|max:255',
-        'customer_email' => 'required|email|max:255',
-        'customer_phone_number' => 'required|max:30',
-        'address' => 'required|max:60000'
-    ];
 }
