1e768e088fa3ce752e5831b19c92b77ef15e9556
Borrellsss <edoardofilippo.borrello@gmail.com>
2022-10-13 11:20:06 +0200

    add more chart style options

--- resources/views/admin/statistics.blade.php
+++ resources/views/admin/statistics.blade.php
@@ -24,9 +24,9 @@
 
         // cart labels
         const labels = <?php echo json_encode(array_reverse($timestamps)); ?>;
-        
-        // charts data
-        const data = {
+
+        // charts data1
+        const data1 = {
             labels: labels,
             datasets: [
                 {
@@ -48,10 +48,41 @@
             ]
         }
 
+        // charts data2
+        const data2 = {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'Ordini',
+                    backgroundColor: 'rgba(254, 170, 2, 0.55)',
+                    borderColor: 'rgb(254, 170, 2)',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'rgb(254, 170, 2)',
+                    data: <?php echo json_encode($monthly_orders); ?>,
+                }
+            ]
+        }
+
+        // charts data3
+        const data3 = {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'Ordini',
+                    backgroundColor: 'rgba(254, 170, 2, 0.55)',
+                    borderColor: 'rgb(254, 170, 2)',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'rgb(254, 170, 2)',
+                    data: <?php echo json_encode($monthly_orders); ?>,
+                }
+            ]
+        }
+
         // charts configurations
+        // bar chart
         const config1 = {
             type: 'bar',
-            data: data,
+            data: data1,
             options: {
                 maintainAspectRatio: false,
                 scales: {
@@ -69,9 +100,10 @@
             }
         }
 
+        // line chart
         const config2 = {
             type: 'line',
-            data: data,
+            data: data1,
             options: {
                 maintainAspectRatio: false,
                 scales: {
@@ -87,8 +119,72 @@
                     }
                 }
             }
-        };
+        }
 
+        // radar chart
+        const config3 = {
+            type: 'radar',
+            data: data2,
+            options: {
+                maintainAspectRatio: false,
+                scales: {
+                    x: {
+                        title: {
+                            color: 'rgb(254, 170, 2)',
+                            display: true,
+                            text: 'Ultimi 12 mesi'
+                        },
+                        ticks: {
+                            color: 'rgb(116, 6, 2)',
+                        },
+                    }
+                }
+            }
+        }
+
+        // polarArea chart
+        const config4 = {
+            type: 'polarArea',
+            data: data3,
+            options: {
+                maintainAspectRatio: false,
+                scales: {
+                    x: {
+                        title: {
+                            color: 'rgb(254, 170, 2)',
+                            display: true,
+                            text: 'Ultimi 12 mesi'
+                        },
+                        ticks: {
+                            color: 'rgb(116, 6, 2)',
+                        },
+                    }
+                }
+            }
+        }
+
+        // doughnut chart
+        const config5 = {
+            type: 'doughnut',
+            data: data3,
+            options: {
+                maintainAspectRatio: false,
+                scales: {
+                    x: {
+                        title: {
+                            color: 'rgb(254, 170, 2)',
+                            display: true,
+                            text: 'Ultimi 12 mesi'
+                        },
+                        ticks: {
+                            color: 'rgb(116, 6, 2)',
+                        },
+                    }
+                }
+            }
+        }
+
+        // default chart render
         let myChart = new Chart(
             document.getElementById('ms_js-chart'),
             config1
@@ -104,10 +200,25 @@
                     document.getElementById('ms_js-chart'),
                     config1
                 );
-            } else {
+            } else if(chartStyle.value === 'line') {
                 myChart = new Chart(
                     document.getElementById('ms_js-chart'),
                     config2
+                );
+            } else if(chartStyle.value === 'radar') {
+                myChart = new Chart(
+                    document.getElementById('ms_js-chart'),
+                    config3
+                );
+            } else if(chartStyle.value === 'polarArea') {
+                myChart = new Chart(
+                    document.getElementById('ms_js-chart'),
+                    config4
+                );
+            } else if(chartStyle.value === 'doughnut') {
+                myChart = new Chart(
+                    document.getElementById('ms_js-chart'),
+                    config5
                 );
             }
         }
