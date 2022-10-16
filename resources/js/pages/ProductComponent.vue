f5efb20f151f7d834a4b3242928b97b5f485bf9d
jacoposergio <djago32@gmail.com>
2022-10-16 22:26:18 +0200

    fixes done

--- resources/js/pages/ProductComponent.vue
+++ resources/js/pages/ProductComponent.vue
@@ -4,7 +4,7 @@
         <!-- Jumbotron  -->
         <div class="jumbotron jumbotron-fluid">
             <img v-if="user.cover" :src="'storage/' + user.cover" :alt="user.business_name">
-            <img v-else src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" :alt="user.business_name">
+            <img v-else src="https://i.ibb.co/W3NLMbb/fallbackdarkred.jpg" :alt="user.business_name">
             <div class="container d-flex justify-content-around">
                 <div class="restaurant-heading">
 
